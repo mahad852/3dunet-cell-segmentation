cdcf40bb35e28d3e340cdca53e751f0853600a61
mahad852 <mahad852@gmail.com>
2024-09-12 02:06:51 -0400

    changing val dataset

--- create_hd5_dataset.py
+++ create_hd5_dataset.py
@@ -3,5 +3,7 @@ from datasets.CellDataset import CellDataset
 
 # ds = CellDataset(data_path='/Users/mahad/Downloads/Widefield Deconvolved Set 2')
 ds = CellDataset(data_path='/home/mali2/datasets/CellSeg/Widefield Deconvolved Set 2')
-
 convert_torch_dataset_to_h5py(ds, ["raw", "label"], root_dir="/home/mali2/datasets/CellSeg/train/hd5_files_cell")
+
+ds = CellDataset(data_path='/home/mali2/datasets/CellSeg/Widefield Deconvolved')
+convert_torch_dataset_to_h5py(ds, ["raw", "label"], root_dir="/home/mali2/datasets/CellSeg/val/hd5_files_cell")
