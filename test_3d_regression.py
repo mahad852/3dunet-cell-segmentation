c02fe5437464ff72b6535555b5b1a60bc8b89ea6
mahad852 <mahad852@gmail.com>
2024-09-25 16:22:45 -0400

    excluding first and last z-slices in test_regression script

--- test_3d_regression.py
+++ test_3d_regression.py
@@ -109,6 +109,10 @@ def main():
         val_outputs = None
         for val_data in val_loader:
             val_images, val_labels, img_pths = val_data[0].to(device), val_data[1].to(device), val_data[2]
+            
+            val_images = val_images[:, :, 6:20, :, :]
+            val_labels = val_labels[:, :, 6:20, :, :]
+
             roi_size = (16, 512, 512)
             sw_batch_size = 4
             val_outputs = sliding_window_inference(val_images, roi_size, sw_batch_size, model)
