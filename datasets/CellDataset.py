7e9e57946e74961d85b9f39486e521bc8618e3e7
mahad852 <mahad852@gmail.com>
2024-09-29 17:01:47 -0400

    correcting weighted mse

--- datasets/CellDataset.py
+++ datasets/CellDataset.py
@@ -116,17 +116,13 @@ class CellDataset(Dataset):
         if self.num_channels == 1:
             return np.ones(shape=img.shape) / (img.shape[0] * img.shape[1] * img.shape[2])
         else:
-            # tub_mask = self.get_tub_mask(img)
-            # mito_mask = self.get_mito_mask(img)
-            # overlap_mask = mito_mask & tub_mask
-            # weights = np.ones(img.shape[1:])
-            # weights[tub_mask.nonzero()] = 2
-            # weights[overlap_mask.nonzero()] = 3
-            # return weights
-            
-            weights = (self.scale_image(img[0])/255) * 2 + (self.scale_image(img[1])/255)
+            tub_mask = self.get_tub_mask(img)
+            mito_mask = self.get_mito_mask(img)
+            overlap_mask = mito_mask & tub_mask
+            weights = np.ones(img.shape[1:])
+            weights[overlap_mask.nonzero()] = 5
             return weights
-
+            
     def get_mito_image(self, img: np.ndarray) -> np.ndarray:
         return self.scale_image(img[1])
     
