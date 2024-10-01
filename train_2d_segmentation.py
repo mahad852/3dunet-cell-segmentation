de8f3c3f64873888ac280b076b59935a8ea5c183
mahad852 <mahad852@gmail.com>
2024-10-01 16:10:13 -0400

    fixing roi_size for 2d val

--- train_2d_segmentation.py
+++ train_2d_segmentation.py
@@ -140,7 +140,7 @@ def main():
                 val_outputs = None
                 for val_data in val_loader:
                     val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
-                    roi_size = (16, 512, 512)
+                    roi_size = (512, 512)
                     sw_batch_size = 4
                     val_outputs = sliding_window_inference(val_images, roi_size, sw_batch_size, model)
                     val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
