712e13dc291c8bacffc5382777042efab2d8ea09
mahad852 <mahad852@gmail.com>
2024-10-21 19:18:08 -0400

    added default values for boolean flags

--- scripts/train_3d_segmenetation.sh
+++ scripts/train_3d_segmenetation.sh
@@ -1,8 +1,8 @@
 python train.py \
     --mode "3d" \
-    --output-model-path "model/seg_test.pth" \
+    --output_model_path "model/seg_test.pth" \
     --train-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved Set 2" \
     --val-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved" \
-    --output-path "/home/mali2/datasets/CellSeg/generated/seg" \
+    --output_path "/home/mali2/datasets/CellSeg/generated/seg" \
 
 
