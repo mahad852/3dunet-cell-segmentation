8820f05cc1ebbe7dc7412f9a8ca4692030b7ca2c
mahad852 <mahad852@gmail.com>
2024-10-22 02:23:56 -0400

    minor typo fix in train_3d_seg.sh

--- scripts/train_3d_segmenetation.sh
+++ scripts/train_3d_segmenetation.sh
@@ -1,6 +1,6 @@
 python train.py \
     --mode "3d" \
-    --output-model-path "model/seg_test.pth" \
+    --output-model-path "models/seg_test.pth" \
     --train-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved Set 2" \
     --val-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved" \
     --output-path "/home/mali2/datasets/CellSeg/generated/seg" \
