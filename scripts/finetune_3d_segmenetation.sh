57f19fcc4437a6be97ee2eab9361124c3b60ba24
mahad852 <mahad852@gmail.com>
2024-10-22 21:11:05 -0400

    added finetuning bash script

---
+++ scripts/finetune_3d_segmenetation.sh
@@ -0,0 +1,8 @@
+python train.py \
+    --mode "3d" \
+    --input-model-path "models/seg_lfd.pth" \
+    --output-model-path "models/finetuned_seg.pth" \
+    --train-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved Set 2" \
+    --val-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved" \
+    --epochs 10 \
+
