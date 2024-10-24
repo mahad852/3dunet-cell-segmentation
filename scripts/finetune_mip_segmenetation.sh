42da5b480b8ce39d7f817bbeb623dd126327bb59
mahad852 <mahad852@gmail.com>
2024-10-24 15:06:14 -0400

    fixing mip flag in bash scripts

--- scripts/finetune_mip_segmenetation.sh
+++ scripts/finetune_mip_segmenetation.sh
@@ -1,6 +1,6 @@
 python train.py \
     --mode "2d" \
-    --mip \
+    --is-mip \
     --input-model-path "models/seg_lfd_mip.pth" \
     --output-model-path "models/finetuned_seg_mip.pth" \
     --train-ds-path "/home/mali2/datasets/CellSeg/Widefield Deconvolved Set 2" \
