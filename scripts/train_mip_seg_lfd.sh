63a371d16ef564f73f141844becd2b1407cf9e62
mahad852 <mahad852@gmail.com>
2024-10-24 14:00:14 -0400

    fixing mip flag in bash scripts

--- scripts/train_mip_seg_lfd.sh
+++ scripts/train_mip_seg_lfd.sh
@@ -1,6 +1,6 @@
 python train.py \
     --mode "2d" \
-    --mip \
+    --is-mip \
     --output-model-path "models/seg_lfd_mip.pth" \
     --train-ds-path "/home/mali2/datasets/CellSeg/LabelFreeCust/train" \
     --val-ds-path "/home/mali2/datasets/CellSeg/LabelFreeCust/val" \
